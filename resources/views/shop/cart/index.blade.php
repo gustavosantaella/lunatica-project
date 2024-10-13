88e94ce690bb661f6f621b2c71765d4a0392c108
Luigi Hernandez <67715584+Luigi019@users.noreply.github.com>
2024-10-13 14:19:47 -0400

    fix

--- resources/views/shop/cart/index.blade.php
+++ resources/views/shop/cart/index.blade.php
@@ -1,36 +1,41 @@
 @extends('layouts.shop-layout')
 
-
 @section('content')
-<table class="table">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>PRODUCTO</th>
-            <th>CANTIDAD</th>
-            <th>PRECIO</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        @foreach ($products as $key => $product )
+    <table class="table">
+        <thead>
             <tr>
-                <th> {{ $key }} </th>
-                <td> {{ $product->nombre }} </td>
-                <td> {{ $product->cantidad }} </td>
-                <td> {{ $product->precio }} </td>
+                <th>ID</th>
+                <th>PRODUCTO</th>
+                <th>CANTIDAD</th>
+                <th>PRECIO</th>
             </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-        <tr>
-            <th style="text-align:right !important" colspan="3">TOTAL</th>
-            <th>100$</th>
-        </tr>
-    </tfoot>
-</table>
+        </thead>
 
-<form action="#">
-    <button class="btn btn-primary">Comprar</button>
-</form>
+        <tbody>
+            @php
+                $total = 0;
+            @endphp
+            @foreach (session('car', []) as $key => $product)
+                <tr>
+                    <th> {{ $key }} </th>
+                    <td> {{ $product['nombre'] }} </td>
+                    <td> {{ $product['quantity'] }} </td>
+                    <td> {{ $product['precio'] }} </td>
+                </tr>
+                @php
+                    $total += $product['precio'] * $product['quantity'];
+                @endphp
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <th style="text-align:right !important" colspan="3">TOTAL</th>
+                <th>{{ $total }}$</th>
+            </tr>
+        </tfoot>
+    </table>
+
+    <form action="#">
+        <button class="btn btn-primary">Comprar</button>
+    </form>
 @endsection
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,41 +1,36 @@
 @extends('layouts.shop-layout')
 
+
 @section('content')
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>PRODUCTO</th>
-                <th>CANTIDAD</th>
-                <th>PRECIO</th>
-            </tr>
-        </thead>
+    <div class="">
 
-        <tbody>
-            @php
-                $total = 0;
-            @endphp
-            @foreach (session('car', []) as $key => $product)
-                <tr>
-                    <th> {{ $key }} </th>
-                    <td> {{ $product['nombre'] }} </td>
-                    <td> {{ $product['quantity'] }} </td>
-                    <td> {{ $product['precio'] }} </td>
-                </tr>
-                @php
-                    $total += $product['precio'] * $product['quantity'];
-                @endphp
+        <div class="height d-flex justify-content-center align-items-center flex-wrap">
+            @foreach ($products as $product)
+                <div class="card m-2 col-md-4">
+                    <img class="card-img-top" src="{{ $product->foto ?? 'https://i.imgur.com/MGorDUi.png' }}" width="200">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h1 class="  ">{{ $product->nombre }}</h1>
+                        </div>
+                        <div class="card-text">
+                            <p> {{ $product->descripcion ?? 'Una excelente opcion para vestir!' }}</p>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="card-text mb-3">Cantidad disponible: {{ $product->cantidad }}</div>
+                        <form action="{{ route('car.store') }}" method="POST"
+                            style="display:inline-block; margin-left: 10px;">
+                            @csrf
+                            <input type="hidden" name="product_id" value="{{ $product->id }}">
+                            <input type="hidden" name="quantity" value="1">
+                            <button type="submit" class="btn btn-danger">Añadir al carrito</button>
+                        </form>
+                    </div>
+
+                </div>
             @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th style="text-align:right !important" colspan="3">TOTAL</th>
-                <th>{{ $total }}$</th>
-            </tr>
-        </tfoot>
-    </table>
+        </div>
 
-    <form action="#">
-        <button class="btn btn-primary">Comprar</button>
-    </form>
+    </div>
 @endsection
